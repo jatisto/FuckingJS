bbbe2d5cfee2fe3c35012a552c5a47f9a41d09d8
Joni <j.atisto@gmail.com>
2018-10-16 13:14:03 +0600

    3TestMade

---
+++ JsTest/script.js
@@ -0,0 +1,62 @@
+// alert('Задание 1');
+//
+//
+// var arr = ['Кефир','Водка','Водка','Кефир','Кефир' ]
+//
+// for (i = 0; i < arr.length; i++){
+//     if (arr[i] === 'Кефир')
+//     {
+//         console.log(arr[i], 'это хорошо');
+//     }
+//     else
+//     {
+//         console.log(arr[i], 'это плохо');
+//     }
+// }
+//
+//
+// //----------------------------------------------------
+//
+// alert('Задание 2');
+//
+// var array = [1, 5, 12, 4, 3];
+//
+// for (i = 0; i < array.length; i++){
+//
+//     if (i < 3){
+//         console.log(number(array[i] + 2));
+//     }
+//     else {
+//        console.log(number(array[i] + 3));
+//     }
+// }
+// number()
+//
+//  function number (num) {
+//     return num;
+//
+// }
+//
+// //----------------------------------------------------
+//
+// alert('Задание 3');
+
+// var result  = prompt("Кто лучший друг крокодила Гены?", '');
+//
+// function cheburash()
+// {
+//     if (result === 'чебураш')
+//     {
+//         alert('And this right!');
+//     }
+//     else
+//     {
+//         alert('And this not right!');
+//     }
+// }
+//
+// cheburash(result);
+
+//----------------------------------------------------
+
+alert('Задание 4');
